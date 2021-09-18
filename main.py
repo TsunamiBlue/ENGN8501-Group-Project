78887505ef2b214bee620c5836c5116b79019e3e
Jerry Zheng <u6579712@anu.edu.au>
2021-09-18 21:58:36 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import torch
+import torchvision.transforms as T
+import models
+from torch.utils.data import dataloader,DataLoader
+from tqdm import tqdm
+
+
+
+
+def main():
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+
+    ### data pre-processing
+
+
+
+
+
+    ### hyper-parameters
+
+
+
+    ### training
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
